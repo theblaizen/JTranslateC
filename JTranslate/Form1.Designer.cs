a1c8a6700d8b48ef9bb8189dd974b260e006301d
Jaroslaw Owdienko <jaroslaw.owdiyenko@ex.ua>
2016-05-19 19:32:58 +0300

    first commit

---
+++ JTranslate/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace JTranslate
+{
+    partial class JTranslateForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JTranslateForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lbPath = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbWords = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbNotTranslated = new System.Windows.Forms.Label();
+            this.lbOriginalWords = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.cb = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnMoveTo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose file:";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(82, 13);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(92, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(13, 110);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(94, 23);
+            this.btnTranslate.TabIndex = 2;
+            this.btnTranslate.Text = "Translate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(119, 73);
+            this.tb1.Multiline = true;
+            this.tb1.Name = "tb1";
+            this.tb1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb1.Size = new System.Drawing.Size(389, 424);
+            this.tb1.TabIndex = 3;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(548, 73);
+            this.tb2.Multiline = true;
+            this.tb2.Name = "tb2";
+            this.tb2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb2.Size = new System.Drawing.Size(380, 424);
+            this.tb2.TabIndex = 4;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(13, 149);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear fields";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lbPath
+            // 
+            this.lbPath.AutoSize = true;
+            this.lbPath.Location = new System.Drawing.Point(196, 18);
+            this.lbPath.Name = "lbPath";
+            this.lbPath.Size = new System.Drawing.Size(0, 13);
+            this.lbPath.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Words in dictionary";
+            // 
+            // lbWords
+            // 
+            this.lbWords.AutoSize = true;
+            this.lbWords.Location = new System.Drawing.Point(39, 219);
+            this.lbWords.Name = "lbWords";
+            this.lbWords.Size = new System.Drawing.Size(35, 13);
+            this.lbWords.TabIndex = 8;
+            this.lbWords.Text = "label3";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(15, 474);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(92, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save translate";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(581, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Not translated words: ";
+            // 
+            // lbNotTranslated
+            // 
+            this.lbNotTranslated.AutoSize = true;
+            this.lbNotTranslated.Location = new System.Drawing.Point(697, 54);
+            this.lbNotTranslated.Name = "lbNotTranslated";
+            this.lbNotTranslated.Size = new System.Drawing.Size(0, 13);
+            this.lbNotTranslated.TabIndex = 11;
+            // 
+            // lbOriginalWords
+            // 
+            this.lbOriginalWords.AutoSize = true;
+            this.lbOriginalWords.Location = new System.Drawing.Point(211, 54);
+            this.lbOriginalWords.Name = "lbOriginalWords";
+            this.lbOriginalWords.Size = new System.Drawing.Size(0, 13);
+            this.lbOriginalWords.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(162, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Words: ";
+            // 
+            // cb
+            // 
+            this.cb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb.FormattingEnabled = true;
+            this.cb.Location = new System.Drawing.Point(13, 73);
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(94, 21);
+            this.cb.TabIndex = 14;
+            this.cb.SelectedIndexChanged += new System.EventHandler(this.cb_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Select language:";
+            // 
+            // btnMoveTo
+            // 
+            this.btnMoveTo.Location = new System.Drawing.Point(514, 269);
+            this.btnMoveTo.Name = "btnMoveTo";
+            this.btnMoveTo.Size = new System.Drawing.Size(27, 23);
+            this.btnMoveTo.TabIndex = 16;
+            this.btnMoveTo.Text = "<<";
+            this.btnMoveTo.UseVisualStyleBackColor = true;
+            this.btnMoveTo.Click += new System.EventHandler(this.btnMoveTo_Click);
+            // 
+            // JTranslateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 506);
+            this.Controls.Add(this.btnMoveTo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cb);
+            this.Controls.Add(this.lbOriginalWords);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbNotTranslated);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lbWords);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbPath);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "JTranslateForm";
+            this.Text = "JTranslate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lbPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbWords;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbNotTranslated;
+        private System.Windows.Forms.Label lbOriginalWords;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ComboBox cb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnMoveTo;
+    }
+}
+
---
+++ JTranslate/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace JTranslate
+{
+    public partial class JTranslateForm : Form
+    {
+        private Dictionary<string, string> dictionary_eng_por = new Dictionary<string, string>();
+        private Dictionary<string, string> dictionary_por_eng = new Dictionary<string, string>();
+
+        private long words_indict_; //count of dictinary words
+        private long not_translated_words_; //count of not translated words
+        private long words_original_; // count of words original
+        private bool change_method; // seting eng/por or por/eng dictionary
+
+        public JTranslateForm()
+        {
+            InitializeComponent();
+            readFromFile();
+            Install();
+        }
+
+
+        //pre init.
+        private void Install()
+        {
+            lbWords.Text = words_indict_.ToString();
+            cb.Items.Add("Eng/Por");
+            cb.Items.Add("Por/Eng");
+            cb.SelectedIndex = 0;
+        }
+
+
+        /**************************_EVENTS_*********************************/
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tb1.Text = "";
+            tb2.Text = "";
+            lbPath.Text = "";
+            lbNotTranslated.Text = "";
+            lbOriginalWords.Text = "";
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+
+            openFileDialog1.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.FileName = "";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            string path = openFileDialog1.FileName;
+                            tb1.Text = System.IO.File.ReadAllText(path);
+                            lbPath.Text = path;
+
+                            string original_w = tb1.Text;
+                            string[] separators = { ",", ".", "!", "?", ";", ":", " " };
+                            string[] words_count = original_w.Split(separators, StringSplitOptions.RemoveEmptyEntries); ;
+
+                            for (int i = 0; i < words_count.Length; i++)
+                                words_original_++;
+
+                            lbOriginalWords.Text = words_original_.ToString();
+                            words_count = null;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(tb1.Text))
+                MessageBox.Show("Nothing to translate.", "Error #87", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                translate();
+                not_translated_words_ = 0;
+            }
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(tb1.Text))
+                MessageBox.Show("Nothing to save.", "Error #89", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                saveFileDialog1.Title = "Save translation";
+                saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog1.FilterIndex = 1;
+                saveFileDialog1.ShowDialog();
+
+                if (saveFileDialog1.FileName != "")
+                {
+                    using (StreamWriter sw = new StreamWriter(saveFileDialog1.OpenFile()))
+                    {
+                        sw.Write(tb2.Text);
+                        sw.Close();
+                    }
+                }
+            }
+        }
+
+        private void btnMoveTo_Click(object sender, EventArgs e)
+        {
+            string buff = tb2.Text;
+            tb1.Text = buff;
+            tb2.Text = "";
+        }
+
+
+        /*************************_METHODS_*******************************/
+        private void cb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cb.SelectedItem.ToString().Equals("Eng/Por"))
+                change_method = true;
+            else
+                change_method = false;
+        }
+
+        private string capitalizeFirstLetter(string fresh, string original)
+        {
+            string original_copy = original.ToLower();
+
+            if (original.Substring(0, 1).Equals(original_copy.Substring(0, 1)))
+                return fresh;
+            if (!original.Substring(0, 1).Equals(original_copy.Substring(0, 1)))
+                return fresh.Substring(0, 1).ToUpper() + fresh.Substring(1);
+
+            return fresh;
+        }
+        
+        private void readFromFile()
+        {
+            string line = "", line2 = "";
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader("data/dict.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                line2 = file.ReadLine();
+
+                if (!dictionary_eng_por.ContainsKey(line))
+                    dictionary_eng_por.Add(line, line2);
+
+                if (!dictionary_por_eng.ContainsKey(line2))
+                    dictionary_por_eng.Add(line2, line);
+                
+                words_indict_++;
+            }
+
+            file.Close();
+        }
+
+        private void translate()
+        {
+            string original = tb1.Text, wrap = "", last_ = "", buff = "";
+            string[] words_original = original.Split(' ');
+
+            Dictionary<string, string> dict;
+            if (change_method)
+                dict = dictionary_eng_por;
+            else
+                dict = dictionary_por_eng;
+
+            for (int i = 0; i < words_original.Length; i++)
+            {
+                wrap = words_original[i].ToLower();
+
+                if (wrap.Length == 0) continue;
+
+                if (dict.ContainsKey(wrap))
+                {
+                    buff = capitalizeFirstLetter(dict[wrap], words_original[i]);
+                    last_ += buff + " ";
+                }
+                else if ( words_original[i][words_original[i].Length - 1] == '.' || words_original[i][words_original[i].Length - 1] == ','
+                        || words_original[i][words_original[i].Length - 1] == '!' || words_original[i][words_original[i].Length - 1] == '?'
+                        || words_original[i][words_original[i].Length - 1] == '-' || words_original[i][words_original[i].Length - 1] == ':'
+                        || words_original[i][words_original[i].Length - 1] == ';' )
+                {
+                    if (dict.ContainsKey(words_original[i].Substring(0, words_original[i].Length - 1)))
+                    {
+                        buff = capitalizeFirstLetter(dict[words_original[i].Substring(0, words_original[i].Length - 1)], 
+                            words_original[i].Substring(0, words_original[i].Length - 1));
+                        last_ += buff + words_original[i][words_original[i].Length - 1] + " " + " ";
+                    }
+                    else
+                    {
+                        buff = capitalizeFirstLetter(words_original[i], words_original[i]);
+                        last_ += buff + " ";
+                        not_translated_words_++;
+                    }
+                }
+                else
+                {
+                    buff = capitalizeFirstLetter(words_original[i], words_original[i]);
+                    last_ += buff + " ";
+                    not_translated_words_++;
+                }
+            }
+
+            last_ = Regex.Replace(last_, @"\s+", " ");
+            tb2.Text = last_;
+            lbNotTranslated.Text = not_translated_words_.ToString();
+        }
+
+    }
+}
